2d00ce0aa7928cc2943933b04d9a832d1e591807
Anna <anna2.00@mail.ru>
2020-03-19 17:16:27 +0300

    clean main activity

--- app/src/main/java/com/loftblog/loftmoney/MainActivity.java
+++ app/src/main/java/com/loftblog/loftmoney/MainActivity.java
@@ -15,14 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.btnMainNext).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent secondActivity = new Intent(getApplicationContext(), SecondActivity.class);
-                secondActivity.putExtra ( name:"Title", value:"Iron man");
-                startActivity(secondActivity);
-            }
-        });
+
     }
 }
 
