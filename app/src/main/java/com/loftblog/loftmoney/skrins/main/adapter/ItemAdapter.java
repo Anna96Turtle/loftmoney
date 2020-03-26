4043ced4ef493fcacaa3a98ae624b920d798e1b2
Анна Овчинникова <55911622+Anna96Turtle@users.noreply.github.com>
2020-03-26 22:55:14 +0300

    Update ItemAdapter.java

--- app/src/main/java/com/loftblog/loftmoney/skrins/main/adapter/ItemAdapter.java
+++ app/src/main/java/com/loftblog/loftmoney/skrins/main/adapter/ItemAdapter.java
@@ -14,50 +14,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder> {
+    private List<Item> mDataList = new ArrayList<>();
 
-private List<Item> mDataList = new ArrayList<>();
-
-  public void setNewData(List<Item> newData) {
-      mDataList.clear();
-      mDataList.addAll(newData);
-      notifyDataSetChanged();
+    public void setNewData(List<Item> newData) {
+        mDataList.clear();
+        mDataList.addAll(newData);
+        notifyDataSetChanged();
     }
 
-  public void addDataToTop(Item model){
-      mDataList.add(0, model);
-      notifyItemChanged(0);
+    public void addDataToTop(Item model) {
+        mDataList.add(0, model);
+        notifyItemChanged(0);
     }
 
-@NonNull
-@Override
+
+    @NonNull
+    @Override
     public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-      LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-      return new ItemViewHolder(layoutInflater.inflate(R.layout.cell_charge, parent,false));
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        return new ItemViewHolder(layoutInflater.inflate(R.layout.cell_charge, parent, false));
     }
 
-@Override
+    @Override
     public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
-      holder.bind(mDataList.get(position));
+        holder.bind(mDataList.get(position));
     }
 
-@Override
+    @Override
     public int getItemCount() {
-      return mDataList.size();
+        return mDataList.size();
     }
 
     static class ItemViewHolder extends RecyclerView.ViewHolder {
-      private TextView txtName = itemView.findViewById(R.id.txtItemName);
-      private TextView txtVelue = itemView.findViewById(R.id.txtItemValue);
+        private TextView txtName = itemView.findViewById(R.id.txtItemName);
+        private TextView txtVelue = itemView.findViewById(R.id.txtItemValue);
 
-      ItemViewHolder(@NonNull View itemView) {
-      super(itemView);
+        ItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        void bind(Item Item) {
+            txtName.setText(Item.getName());
+            txtVelue.setText(Item.getValue());
+        }
+
     }
-
-    void bind(Item Item){
-      txtName.setText(Item.getName());
-      txtVelue.setText(Item.getValue());
-    }
-
-  }
 
 }
